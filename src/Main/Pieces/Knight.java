da4631b68b23a637c286906612cde8491864060d
Alexander Carlisi <alexandercarlisi0@gmail.com>
2024-01-13 16:52:37 -0500

    add knight

---
+++ src/Main/Pieces/Knight.java
@@ -0,0 +1,31 @@
+package Main.Pieces;
+
+import java.util.ArrayList;
+import Main.Chess;
+import Main.Chess.Move;
+
+
+/**
+* Pawn piece, extends the Piece superclass
+*/
+public class Knight extends Piece {
+    
+    // Constructor
+    public Knight(Piece.Team team, int x, int y) {
+        super(team);
+        setPosition(x, y);
+    }
+    
+    
+    @Override
+    public ArrayList<Move> getAvailableMoves() {
+        
+        return null;
+    }
+    
+    
+    @Override
+    public String getName() {
+        return "Knight";
+    }
+}
